697adccd3b2a5f139692604149aa8f911231264e
HP <jawad0318>
2022-02-20 13:41:12 +0500

    login

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btn2);
+                button.hasOnClickListeners(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v){
+                        openActivity_projectlogin();
+
+                    }
+
+                });
+    }
+    public void openActivity_projectlogin(){
+        Intent intent = new Intent(this, openActivity_projectlogin();
+        startActivity(intent);
     }
 }
